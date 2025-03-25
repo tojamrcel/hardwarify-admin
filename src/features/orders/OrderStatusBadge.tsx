07002f3e38b2fe8dbb697a9297d3f7b526feb35f
tojamrcel <marcelgala42@gmail.com>
2025-03-25 19:31:33 +0100

    style: improve styling of OrderStatusBadge

--- src/features/orders/OrderStatusBadge.tsx
+++ src/features/orders/OrderStatusBadge.tsx
@@ -5,7 +5,7 @@ interface OrderStatusBadgeProps {
 function OrderStatusBadge({ status }: OrderStatusBadgeProps) {
   return (
     <span
-      className={`rounded-full ${status === "pending" && "bg-green-500"} ${status === "sent" && "bg-yellow-600"} ${status === "delivered" && "bg-red-700"} px-2 font-semibold tracking-tight text-stone-100`}
+      className={`rounded-full ${status === "pending" && "bg-green-500"} ${status === "sent" && "bg-yellow-600"} ${status === "delivered" && "bg-red-700"} w-24 px-2 text-center font-semibold tracking-tight text-stone-100`}
     >
       {status.toUpperCase()}
     </span>
