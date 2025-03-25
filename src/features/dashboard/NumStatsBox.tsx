5764c06d6780b2b0523f0356aa7008142fe35673
tojamrcel <marcelgala42@gmail.com>
2025-03-25 16:08:48 +0100

    feat: implement boxes with simple stats in dashboard component

---
+++ src/features/dashboard/NumStatsBox.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from "react";
+
+interface NumStatsProps {
+  children: ReactNode;
+  number: number;
+  color: "green" | "yellow" | "red";
+}
+
+function NumStatsBox({ children, number, color }: NumStatsProps) {
+  return (
+    <div className="grid h-24 grid-cols-[auto_1fr] items-center gap-4 rounded-md bg-stone-100 p-4 text-gray-700 shadow-md">
+      <span
+        className={`border-r border-gray-300 px-2 text-4xl font-bold text-green-500 ${color === "green" && "text-green-500"} ${color === "red" && "text-red-700"} ${color === "yellow" && "text-yellow-600"}`}
+      >
+        {number}
+      </span>
+      <p className="text-center text-lg font-semibold">{children}</p>
+    </div>
+  );
+}
+
+export default NumStatsBox;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,22 @@
+import NumStatsBox from "../features/dashboard/NumStatsBox";
+
 function Dashboard() {
-  return <div>dashboard</div>;
+  return (
+    <div className="flex flex-col gap-6">
+      <h1 className="text-2xl font-bold text-gray-600">Dashboard</h1>
+      <div className="grid grid-cols-3 gap-16">
+        <NumStatsBox number={3} color="green">
+          NEW ORDERS TODAY
+        </NumStatsBox>
+        <NumStatsBox number={7} color="yellow">
+          ORDERS WAITING BE SHIPPED
+        </NumStatsBox>
+        <NumStatsBox number={48} color="red">
+          ALL ORDERS
+        </NumStatsBox>
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
