c69fa149c1f70c969f4c10795e1164ab59de5630
tojamrcel <marcelgala42@gmail.com>
2025-03-27 17:25:38 +0100

    feat: create supabase.ts

--- src/App.tsx
+++ src/App.tsx
@@ -7,23 +7,28 @@ import Products from "./pages/Products";
 import Login from "./pages/Login";
 import Layout from "./ui/Layout";
 import AddNewProduct from "./pages/AddNewProduct";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route index element={<Navigate replace to="/dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="orders" element={<Orders />} />
-          <Route path="order/:orderId" element={<Order />} />
-          <Route path="products" element={<Products />} />
-          <Route path="products/add" element={<AddNewProduct />} />
-        </Route>
-        <Route path="login" element={<Login />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route index element={<Navigate replace to="/dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="orders" element={<Orders />} />
+            <Route path="order/:orderId" element={<Order />} />
+            <Route path="products" element={<Products />} />
+            <Route path="products/add" element={<AddNewProduct />} />
+          </Route>
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
---
+++ src/services/supabase.ts
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://oluekkawuggalugtvbig.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9sdWVra2F3dWdnYWx1Z3R2YmlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE3Nzc4MzUsImV4cCI6MjA1NzM1MzgzNX0.cOIPxVbZiQmDn9pGc92IHIaTggyEsBnA0bN4OUz0dvg";
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
