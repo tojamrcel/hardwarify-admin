c1cf356c3bc0985d63398b5ffd61ba67162f5c02
tojamrcel <marcelgala42@gmail.com>
2025-04-18 15:43:51 +0200

    feat: create search box in Products component

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,3 +1,4 @@
+import { HiSearch } from "react-icons/hi";
 import ProductsList from "../features/products/ProductsList";
 import Button from "../ui/Button";
 import Title from "../ui/Title";
@@ -6,6 +7,16 @@ function Products() {
   return (
     <div className="flex max-w-3/4 flex-col gap-2">
       <Title>Products</Title>
+      <div className="relative flex h-10 w-full items-center justify-center">
+        <input
+          type="text"
+          className="transition-full h-10 w-full rounded-full border-2 border-transparent bg-stone-100 px-4 pl-10 text-gray-600 duration-200 placeholder:italic focus:border-red-700 focus:outline-none"
+          placeholder="Search for products..."
+        />
+        <div className="absolute left-4 text-gray-500">
+          <HiSearch />
+        </div>
+      </div>
       <ProductsList />
       <div className="ml-auto">
         <Button link="/products/add" type="secondary">
