176d73a10b6bf80f313407a21772ef07d9a273a9
tojamrcel <marcelgala42@gmail.com>
2025-04-20 11:37:57 +0200

    feat: implement searching for product in products page

--- src/features/products/ProductsList.tsx
+++ src/features/products/ProductsList.tsx
@@ -2,9 +2,29 @@ import ProductItem from "./ProductItem";
 import useProducts from "./useProducts";
 import Menus from "../../ui/Menus";
 import Spinner from "../../ui/Spinner";
+import { useEffect, useState } from "react";
 
-function ProductsList() {
+function ProductsList({ searchValue }: { searchValue: string }) {
   const { products, isLoading, error } = useProducts();
+  const [searchedProducts, setSearchedProducts] = useState(products);
+
+  useEffect(() => {
+    if (searchValue) {
+      const productsFiltered = products
+        ?.map((product) =>
+          product.product_name
+            .toLowerCase()
+            .startsWith(searchValue.toLowerCase())
+            ? product
+            : null,
+        )
+        .filter((prod) => prod !== null);
+
+      setSearchedProducts(productsFiltered);
+    } else if (!searchValue) {
+      setSearchedProducts(products);
+    }
+  }, [products, searchValue, setSearchedProducts]);
 
   if (isLoading)
     return (
@@ -13,10 +33,19 @@ function ProductsList() {
       </div>
     );
 
+  if (!searchedProducts?.length)
+    return (
+      <div className="my-5">
+        <p className="text-center text-lg text-gray-500">
+          We could not find product that you're looking for :(
+        </p>
+      </div>
+    );
+
   return (
     <ul className="mt-2 flex max-w-full flex-col gap-4">
       <Menus>
-        {products?.map((product) => (
+        {searchedProducts?.map((product) => (
           <ProductItem product={product} key={product.id} />
         ))}
       </Menus>
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import { HiSearch } from "react-icons/hi";
 import ProductsList from "../features/products/ProductsList";
 import Button from "../ui/Button";
 import Title from "../ui/Title";
 
 function Products() {
+  const [searchValue, setSearchValue] = useState<string>("");
+
+  function onChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setSearchValue(e.target.value);
+  }
+
   return (
     <div className="flex max-w-3/4 flex-col gap-2">
       <Title>Products</Title>
@@ -12,12 +19,13 @@ function Products() {
           type="text"
           className="transition-full h-10 w-full rounded-full border-2 border-transparent bg-stone-100 px-4 pl-10 text-gray-600 duration-200 placeholder:italic focus:border-red-700 focus:outline-none"
           placeholder="Search for products..."
+          onChange={onChange}
         />
         <div className="absolute left-4 text-gray-500">
           <HiSearch />
         </div>
       </div>
-      <ProductsList />
+      <ProductsList searchValue={searchValue} />
       <div className="ml-auto">
         <Button link="/products/add" type="secondary">
           Add new item
