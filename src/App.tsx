dc4423ec7cf1018a56af3d6f296ad2735c6d16b1
tojamrcel <marcelgala42@gmail.com>
2025-04-24 19:38:30 +0200

    chore: add react query devtools

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,14 @@ import Layout from "./ui/Layout";
 import AddNewProduct from "./pages/AddNewProduct";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Protected from "./ui/Protected";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <BrowserRouter>
         <Routes>
           <Route
