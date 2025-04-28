772d3dad992d8a18ce70bf64858423bc4c189ca6
tojamrcel <marcelgala42@gmail.com>
2025-04-29 00:10:38 +0200

    feat: implement prefetching in useProducts hook

--- src/features/products/useProducts.ts
+++ src/features/products/useProducts.ts
@@ -1,8 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getProducts } from "../../services/apiProducts";
 import { useSearchParams } from "react-router";
+import { PER_PAGE } from "../../utils/constants";
 
 function useProducts(searchTerm: string | undefined) {
+  const queryClient = useQueryClient();
   const [searchParams] = useSearchParams();
   const page = searchParams.get("page") ? Number(searchParams.get("page")) : 1;
 
@@ -14,6 +16,22 @@ function useProducts(searchTerm: string | undefined) {
     queryKey: ["products", searchTerm, page],
     queryFn: () => getProducts({ searchTerm, page }),
   });
+
+  if (data) {
+    const pageCount = Math.ceil(data.count / PER_PAGE);
+
+    if (page < pageCount)
+      queryClient.prefetchQuery({
+        queryKey: ["products", searchTerm, page + 1],
+        queryFn: () => getProducts({ searchTerm, page: page + 1 }),
+      });
+
+    if (page > 1)
+      queryClient.prefetchQuery({
+        queryKey: ["products", searchTerm, page - 1],
+        queryFn: () => getProducts({ searchTerm, page: page - 1 }),
+      });
+  }
 
   return { data, isLoading, error };
 }
