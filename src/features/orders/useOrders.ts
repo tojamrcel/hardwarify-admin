a1ca4291cb80ac631f1a52a2311d83af26e09945
tojamrcel <marcelgala42@gmail.com>
2025-04-29 00:04:17 +0200

    feat: implement prefetching in useOrders hook

--- src/features/orders/useOrders.ts
+++ src/features/orders/useOrders.ts
@@ -1,8 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getOrders } from "../../services/apiOrders";
 import { useSearchParams } from "react-router";
+import { PER_PAGE } from "../../utils/constants";
 
 function useOrders() {
+  const queryClient = useQueryClient();
   const [searchParams] = useSearchParams();
   const curPage = searchParams.get("page")
     ? Number(searchParams.get("page"))
@@ -12,6 +14,22 @@ function useOrders() {
     queryKey: ["orders", curPage],
     queryFn: () => getOrders(curPage),
   });
+
+  if (data) {
+    const pageCount = Math.ceil(data.count / PER_PAGE);
+
+    if (curPage < pageCount)
+      queryClient.prefetchQuery({
+        queryKey: ["orders", curPage + 1],
+        queryFn: () => getOrders(curPage + 1),
+      });
+
+    if (curPage > 1)
+      queryClient.prefetchQuery({
+        queryKey: ["orders", curPage - 1],
+        queryFn: () => getOrders(curPage - 1),
+      });
+  }
 
   return { data, isLoading, error };
 }
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,2 @@
 export const SUPABASE_URL = "https://oluekkawuggalugtvbig.supabase.co";
-export const PER_PAGE = 6;
+export const PER_PAGE = 3;
